bdffbb3cbf87ac79ea6c003081a2f224649e7767
Bored-Wizard <sujanbarman12345q@gmail.com>
2022-07-23 13:06:56 +0530

    added error handlening to the code base

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(greetings.Hello("ok"))
+	// fmt.Println(greetings.Hello("ok"))
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
